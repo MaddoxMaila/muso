2cd560d9cecfe03115331b477e5e59e7e8cac767
MaddoxMaila <maddoxmaila719@gmail.com>
2023-01-15 08:51:47 +0200

    to add playlists

---
+++ controllers/playlists/PlaylistController.ts
@@ -0,0 +1,15 @@
+import { Response, Request } from "express";
+
+
+
+const PlaylistController = {
+    createPlaylist: async (request: Request, response: Response) => {
+        
+    },
+    addTrackToPlaylist: async (request: Request, response: Response) => {
+
+    },
+    deletePlaylist: async (request: Request, response: Response) => {
+        
+    }
+}
