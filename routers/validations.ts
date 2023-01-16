a2ce937f80245a7d62331f7971dae487492b5643
MaddoxMaila <maddoxmaila719@gmail.com>
2023-01-16 23:21:16 +0200

    fixed validation for ID

--- routers/validations.ts
+++ routers/validations.ts
@@ -3,5 +3,5 @@ import { ValidationChain, check } from "express-validator"
 export const EMPTY_FIELD: string = "field cannot be empty"
 
 export const validate = (field: string, message: string): ValidationChain => {
-    return check("id").exists().withMessage("supply a track id").notEmpty().withMessage(`field "${field} cannot be empty"`)
+    return check(field).exists().withMessage(`supply this field "${field}"`).notEmpty().withMessage(`field "${field}" cannot be empty`)
 }
