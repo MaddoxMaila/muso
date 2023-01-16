177dbbe3f1d2ee32f3e87841494963177d694bec
MaddoxMaila <maddoxmaila719@gmail.com>
2023-01-16 21:47:34 +0200

    commented error logging

--- libs/ApiResponse.ts
+++ libs/ApiResponse.ts
@@ -1,7 +1,10 @@
 export default function ApiResponse<T>(error: boolean, message: string, response: T) {
+    
     if(response instanceof Error){
+        // Only print Errors
         console.log(response)
     }
+
     return {
         error, message, ...response
     }
